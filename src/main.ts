fe2f304a0eda2c8fdadafeb5aece752feb83d39c
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-10-07 18:10:43 +0530

    new

--- src/main.ts
+++ src/main.ts
@@ -9,28 +9,54 @@ config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-        exceptionFactory: (errors: ValidationError[]) => {
-          const messages = errors.flatMap(err =>
-            Object.values(err.constraints ?? {})
-          );
+  //   app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //     transform: true,
+  //     exceptionFactory: (errors: ValidationError[]) => {
+  //       const messages = errors.reduce((acc, err) => {
+  //         if (err.constraints) {
+  //           acc[err.property] = Object.values(err.constraints);
+  //         }
+  //         return acc;
+  //       }, {} as Record<string, string[]>);
 
-          return new HttpException(
-            {
-              status: false,
-              status_code: 422,
-              message: messages,
-            },
-            422,
-          );
-        },
-      }),
-    );
+  //       return new HttpException(
+  //         {
+  //           status: false,
+  //           status_code: 422,
+  //           message: messages,
+  //         },
+  //         422,
+  //       );
+  //     },
+  //   }),
+  // );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = errors.flatMap(err =>
+          Object.values(err.constraints ?? {}).map(message => ({
+            message: message
+          }))
+        );
 
+        return new HttpException(
+          {
+            success: false,
+            message: 'Validation failed',
+            data: messages,
+            status_code: 422,
+          },
+          422,
+        );
+      },
+    }),
+  );
   const port = process.env.PORT ?? 3000;
 
   await app.listen(port);
